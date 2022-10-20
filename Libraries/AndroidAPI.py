3853595054b9066ea874e9a1fc00b4adafcb5d2e
gialuong <gialuong9820@gmail.com>
2022-10-21 01:13:53 +0700

    [fix+feat]: add to_dict and fix parse_android

--- Libraries/AndroidAPI.py
+++ Libraries/AndroidAPI.py
@@ -7,7 +7,16 @@ class AndroidAPI:
         self.package = package 
         self.method_name = method_name
         self.full_api_call = package + ";->" + method_name
+
+    def to_dict(self):
+        return {
+            "invoke": self.invoke,
+            "package": self.package,
+            "method_name": self.method_name,
+            "full_api_call": self.full_api_call
+        }
     
+    # ============== static =====================
     def parse(string_api: str):
         string_api = string_api.strip()
         if AndroidAPI.__is_api(string_api) == False:
@@ -35,12 +44,15 @@ class AndroidAPI:
                 method_name = method_name + string_api[i]
         return AndroidAPI(True, invoke, package, method_name)
     
-    def pares_android(string_api: str):
-        if string_api.find("Landoird") == -1 and string_api.find("Ljava") == -1:
-            return AndroidAPI(is_api=False)
+    def parse_android(string_api: str):
+        api = AndroidAPI.parse(string_api=string_api)
+        if api.package.startswith("Landroid") or api.package.startswith("Ljava"):
+            return api
         else:
-            return AndroidAPI.parse(string_api)
+            return AndroidAPI(is_api=False)
 
+
+    #================ private =====================
     def __is_api(string_api: str):
         if re.search(r'^invoke', string_api):
             return True
