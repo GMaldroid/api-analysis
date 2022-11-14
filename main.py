1a90ffd97b7925002b48ee4c73e9a69175b4cb3c
gialuong <gialuong9820@gmail.com>
2022-11-14 19:23:19 +0700

    feat: filter and get top api

--- main.py
+++ main.py
@@ -93,12 +93,83 @@ def transform():
         })
     result.sort(key=lambda x: x["count"], reverse=True)
     result = {
-        "type": "SMS",
+        "type": "smsmalware",
         "data": result
     }
     open("./output/SMS.json", "w").write(json.dumps(result, indent=4))
     pass
 
+def filter():
+    adware = json.load(open("./output/number-of-call/Adware.json", "r"))
+    banking = json.load(open("./output/number-of-call/Banking.json", "r"))
+    benign = json.load(open("./output/number-of-call/Benign.json", "r"))
+    riskware = json.load(open("./output/number-of-call/Riskware.json", "r"))
+    sms = json.load(open("./output/number-of-call/SMS.json", "r"))
+
+    adware_name = [f["name"] for f in adware["data"]]
+    banking_name = [f["name"] for f in banking["data"]]
+    benign_name = [f["name"] for f in benign["data"]]
+    riskware_name = [f["name"] for f in riskware["data"]]
+    sms_name = [f["name"] for f in sms["data"]]
+    
+    adware_result = []
+    banking_result = []
+    bening_result = []
+    riskware_result = []
+    sms_result = []
+
+    print("riskware")
+    sms_nm = []
+    for api in sms_name:
+        if (api not in adware_name) and (api not in banking_name) and (api not in benign_name) and (api not in riskware_name):
+            sms_nm.append(api)
+    print(f"size of sms not match {len(sms_nm)}")
+    for api in sms["data"]:
+        if api["name"] in sms_nm:
+            sms_result.append(api)
+    open("./output/api-not-match/SMS.json", "w").write(
+        json.dumps(
+            {
+                "description": "Top API call only on smsmalware",
+                "type": "smsmalware", 
+                "data": sms_result
+            }
+            , indent=4
+        )
+    )
+
+def topapi():
+    adware = json.load(open("./output/api-not-match/Adware.json", "r"))["data"]
+    banking = json.load(open("./output/api-not-match/Banking.json", "r"))["data"]
+    bening = json.load(open("./output/api-not-match/Banking.json", "r"))["data"]
+    riskware = json.load(open("./output/api-not-match/riskware.json", "r"))["data"]
+    smsmalware = json.load(open("./output/api-not-match/SMS.json", "r"))["data"]
+
+    top_api = []
+
+    for i in range(200):
+        top_api.append(adware[i]["name"])
+        top_api.append(banking[i]["name"])
+        top_api.append(bening[i]["name"])
+        top_api.append(riskware[i]["name"])
+        top_api.append(smsmalware[i]["name"])
+    
+    top_api.sort()
+
+    top_api = {
+        "description": "Top 1000 api",
+        "count": len(top_api),
+        "data": top_api
+    }
+    
+    open("./output/top-api/top1000.json", "w").write(json.dumps(top_api, indent=4))
+
+    
+    print(top_api)
+
+def selection():
+    pass
+    
 if __name__ == '__main__':
     if (len(sys.argv) == 1):
         print('missing argument')
@@ -109,3 +180,9 @@ if __name__ == '__main__':
         total()
     if sys.argv[1] == 'transform':
         transform()
+    if sys.argv[1] == 'filter':
+        filter()
+    if sys.argv[1] == 'topapi':
+        topapi()
+    if sys.argv[1] == 'selection':
+        selection()
